8ad3edde22327e389a8141e5d90c32e3c5413158
ardhen07 <mailardhendu@gmail.com>
2019-09-03 19:39:03 +0530

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public baseUrl ="http://localhost:8080";
+  products: any[];
 
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 
+  get_products(){
+    const headers = new HttpHeaders({ 'Content-Type': 'text/plain'});
+    this.httpClient.get(this.baseUrl + '/hic/allusers').subscribe((res : any[])=>{
+    console.log(res);
+    this.products = res;
+    });
+}
 }
